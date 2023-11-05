ce87b23e80e352389d3586f741f65d19bd984143
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-11-04 20:06:11 -0400

    ya rab

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     i = chr
-
     for i in range(ord('a'), ord('z') + 1):
-                   if i == ord(c):
-                       return 1
+        if i == ord(c):
+            return 1
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(strr):
+    i = chr
+    for i in strr:
+        if (ord(i) >= ord('a')) and (ord(i) <= ord('z')):
+            i = chr(ord(i) - 32)
+            print("{:s}".format(i), end="")
+        print()
