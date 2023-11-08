bdafc478e0b0cf80531fb9c4a5c26b8720bb8bff
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-11-08 07:17:04 -0500

    lol

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(s):
-    if s == None:
-        return  (0, 'None')
+    if s is None:
+        return (0, 'None')
     else:
         return (len(s), s[0])
-
