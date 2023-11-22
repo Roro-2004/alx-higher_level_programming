0171388dfb90ee682be6ea2aed7e58d0c2309366
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-11-21 19:50:37 -0500

    lol

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""import module."""
+
+
+class Square:
+    """cerate a class"""
+
+    def __init__(self, size=0):
+        """ 
+    constructor.
+    
+        Args:
+            size: size of sqaure
+
+        Raises:
+
+            TypeError: size not int
+            ValueError: size < 0
+            """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >=0')
+        self.__size = size
