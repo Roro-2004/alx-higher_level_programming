861581b65990e06c35996ea57a0ed101cec6649d
Roro-2004 <roaa.m.lotfy@gmail.com>
2024-02-28 05:44:22 +0200

    t7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -67,7 +67,7 @@ class Rectangle:
         if (self.__height != 0 and self.__width != 0):
             for i in range(self.__height):
                 for j in range(self.__width):
-                    str += Rectangle.print_symbol
+                    str += self.print_symbol
                 if (i != self.__height - 1):
                     str += "\n"
         return str
